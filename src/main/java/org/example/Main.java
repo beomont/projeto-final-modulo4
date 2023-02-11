e9909e53092a269992764f78aae2faa3c61ed99b
reotsugua <renanaugusto.ras@gmail.com>
2023-02-11 12:14:46 -0300

    Deixando mais organizado

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,10 +8,10 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        GolsCampeonatoView.imprimeGolsAtleta();
+        CartoesCampeonato.mostrarJogadoresComMaisCartoes();
         MaiorPlacarGols maiorPlacarGols = new MaiorPlacarGols();
         maiorPlacarGols.imprimirDados();
-        CartoesCampeonato.mostrarJogadoresComMaisCartoes();
-        GolsCampeonatoView.imprimeGolsAtleta();
 
 
 
--- src/main/java/org/example/view/GolsCampeonatoView.java
+++ src/main/java/org/example/view/GolsCampeonatoView.java
@@ -13,13 +13,15 @@ public class GolsCampeonatoView {
         GolsCampeonatoController golsCampeonatoController = new GolsCampeonatoController();
         System.out.printf("O atleta %s obteve a maior pontuação, com %d Gols!\n", golsCampeonatoController.filtraAtletaComMaiorGol().getKey(), golsCampeonatoController.filtraAtletaComMaiorGol().getValue());
 
+
         List<Gol> gols = golsCampeonatoController.populaListaGols();
+
+        Map<String, Long> jogadorQueMaisFezGolsPenaltis = golsCampeonatoController.jogadorQueMaisFezGolsPenaltis(gols);
+        System.out.println("Jogador que mais fez gols de pênaltis: " + jogadorQueMaisFezGolsPenaltis.keySet().iterator().next() + " (" + jogadorQueMaisFezGolsPenaltis.values().iterator().next() + " gols)");
 
         Map<String, Long> jogadorQueMaisFezGols = golsCampeonatoController.jogadorQueMaisFezGolsContra(gols);
         System.out.println("Jogador que mais fez gols contra: " + jogadorQueMaisFezGols.keySet().iterator().next() + " (" + jogadorQueMaisFezGols.values().iterator().next() + " gols)");
 
-        Map<String, Long> jogadorQueMaisFezGolsPenaltis = golsCampeonatoController.jogadorQueMaisFezGolsPenaltis(gols);
-        System.out.println("Jogador que mais fez gols de pênaltis: " + jogadorQueMaisFezGolsPenaltis.keySet().iterator().next() + " (" + jogadorQueMaisFezGolsPenaltis.values().iterator().next() + " gols)");
     }
 
 
