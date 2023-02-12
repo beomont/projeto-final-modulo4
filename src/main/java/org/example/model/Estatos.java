037007fdb19c23dc429b20c362ec8b121714fc1b
Agamenon Junior <agamenon_422@hotmail.com>
2023-02-12 13:51:37 -0300

    O Estado que teve menos jogos dentro do período 2003 e 2022

---
+++ src/main/java/org/example/model/Estatos.java
@@ -0,0 +1,43 @@
+package org.example.model;
+
+public enum Estatos {
+    AMAZONAS("Amazonas", "AM"),
+    ALAGOAS("Alagoas", "AL"),
+    ACRE("Acre", "AC"),
+    AMAPA("Amapá", "AP"),
+    BAHIA("Bahia", "BA"),
+    PARA("Pará", "PA"),
+    MATO_GROSSO("Mato Grosso", "MT"),
+    MINAS_GERAIS("Minas Gerais", "MG"),
+    MATO_GROSSO_DO_SUL("Mato Grosso do Sul", "MS"),
+    GOIAS("Goiás", "GO"),
+    MARANHAO("Maranhão", "MA"),
+    RIO_GRANDE_DO_SUL("Rio Grande do Sul", "RS"),
+    TOCANTINS("Tocantins", "TO"),
+    PIAUI("Piauí", "PI"),
+    SAO_PAULO("São Paulo", "SP"),
+    RONDONIA("Rondônia", "RO"),
+    RORAIMA("Roraima", "RR"),
+    PARANA("Paraná", "PR"),
+    CEARA("Ceará", "CE"),
+    PERNAMBUCO("Pernambuco", "PE"),
+    SANTA_CATARINA("Santa Catarina", "SC"),
+    PARAIBA("Paraíba", "PB"),
+    RIO_GRANDE_DO_NORTE("Rio Grande do Norte", "RN"),
+    ESPIRITO_SANTO("Espírito Santo", "ES"),
+    RIO_DE_JANEIRO("Rio de Janeiro", "RJ"),
+    SERGIPE("Sergipe", "SE"),
+    DISTRITO_FEDERAL("Distrito Federal", "DF");
+
+    private String estado;
+    private String sigla;
+    Estatos(String estado, String sigla) {
+        this.estado = estado;
+        this.sigla = sigla;
+    }
+
+    public String getEstado() {return estado;}
+
+    public String getSigla() { return sigla;}
+
+}
